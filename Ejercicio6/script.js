ab4e99d1668771571c9000cbca5c3a165a2163de
Ariana Martin <arianamartinmartinez@gmail.com>
2024-06-07 14:09:27 +0200

    Ejercicio 6 hecho

--- Ejercicio6/script.js
+++ Ejercicio6/script.js
@@ -1,3 +1,3 @@
-/*
-Elimina el elemento que tenga la clase .fn-remove-me.
-*/
+//Elimina el elemento que tenga la clase .fn-remove-me.
+const element = document.querySelector('.fn-remove-me');
+element.remove();
